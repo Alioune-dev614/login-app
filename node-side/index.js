fb7e9a4f6ef602c3f263e9cd2a85b24d0a14b61e
Alioune <seckalioune107@gmail.com>
2025-05-30 19:58:31 +0200

    fix: base de données

--- node-side/index.js
+++ node-side/index.js
@@ -8,7 +8,7 @@ const app= express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGODB_URL);
 
 app.post('/login', (req, res) => {
     const { email, password } = req.body;
